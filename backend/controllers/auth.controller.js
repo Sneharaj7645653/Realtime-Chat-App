10c19858914ff53f45c2f714cb6550f6666f5d8c
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-10-18 20:07:14 +0530

    update profile picture

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,5 +82,18 @@ export const logout = (req, res) => {
 }
 
 export const updateProfile = async (req, res) => {
-    
+    try{
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+        if(!profilePic){
+            return res.status(400).json({msg: "Profile picture is required"});
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+        return res.status(200).json({msg: "Profile updated successfully", user: updatedUser} );
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({ msg: "Server error" });
+    }
 }
