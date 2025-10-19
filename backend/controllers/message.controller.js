d7d317e169c79ebcdb364cf7beac59969efc35a9
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-10-19 13:32:55 +0530

    messaging implemented (not realtime)

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,7 @@
+import User from '../models/user.model.js';
+import Message from '../models/message.model.js';
+import cloudinary from '../lib/cloudinary.js';
+
 export const getUsersForSidebar = async (req, res) => {
     try{
         const loggedInUserId = req.user.id;
@@ -9,3 +13,52 @@ export const getUsersForSidebar = async (req, res) => {
         return res.status(500).json({ msg: "Server error" });
     }
 };
+
+export const getMessages = async (req, res) => {
+    try{
+        const {id:userToChatId} = req.params;
+        const myId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
+            ]
+        })
+        return res.status(200).json(messages );
+
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({ msg: "Server error" });
+    }
+}
+
+export const sendMessage = async (req, res)=>{
+    try{
+        const {text, image } = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imgURL;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imgURL = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            messageText: text,
+            image: imgURL
+        });
+        await newMessage.save();
+
+        //todo: realtime functionality will be added here using sockets.io
+
+        return res.status(201).json(newMessage);
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({ msg: "Server error" });
+    }
+}
